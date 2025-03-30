0d96edb951a0e90c35c919948ae1155a2ef16135
Brambilla05 <brambillagianluca@itis-molinari.eu>
2025-03-30 15:48:36 +0200

    bot funzionante ma da gestire un controllo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const fetch = require('node-fetch'); 
 const conf = JSON.parse(fs.readFileSync('conf.json'));
 const token = conf.key;
-const apiToken = conf.apiToken; 
+const serpApiKey = conf.serpApiKey; 
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -12,20 +12,22 @@ let intervalId = null;
 
 async function fetchResults(team) {
     try {
-        const response = await fetch(`https://api.football-data.org/v2/teams/${team}/matches`, {
-            headers: {
-                'X-Auth-Token': apiToken 
-            }
-        });
+        const response = await fetch(`https://serpapi.com/search.json?q=${encodeURIComponent(team)}&location=austin,+texas,+united+states&api_key=${serpApiKey}`);
         const data = await response.json();
-        if (!data.matches || data.matches.length === 0) {
+
+        const scores = data.sports_results.game_spotlight;
+
+        if (!scores || scores.length === 0) {
             return 'Nessuna partita trovata.';
         }
-        const matches = data.matches.map(match => {
-            return `${match.homeTeam.name} vs ${match.awayTeam.name}: ${match.score.fullTime.home} - ${match.score.fullTime.away}`;
-        }).join('\n');
+console.log(scores.teams);
+ 
+            const homeTeam = scores.teams[0].name; 
+            const awayTeam = scores.teams[1].name; 
+            const homeScore = scores.teams[0].score; 
+            const awayScore = scores.teams[1].score; 
 
-        return matches;
+            return `${homeTeam} ${homeScore} - ${awayScore} ${awayTeam}`;
     } catch (error) {
         console.error('Errore nel recupero dei risultati:', error);
         return 'Errore nel recupero dei risultati.';
@@ -42,7 +44,7 @@ function startSendingResults(chatId) {
             const results = await fetchResults(teamName);
             bot.sendMessage(chatId, results);
         }
-    }, 9000); 
+    }, 5000); 
 }
 
 function stopSendingResults() {
@@ -63,7 +65,7 @@ bot.on("message", (msg) => {
         bot.sendMessage(chatId, "Hai fermato l'invio dei risultati.");
     } else {
         teamName = text; 
-        bot.sendMessage(chatId, `Hai selezionato: ${teamName}. Ora ti invierò i risultati ogni 30 minuti.`);
+        bot.sendMessage(chatId, `Hai selezionato: ${teamName}. Ora ti invierò i risultati ogni 5 minuti.`);
         startSendingResults(chatId); 
     }
 });
